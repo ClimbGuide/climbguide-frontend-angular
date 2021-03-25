e35996608c42ff1a635538565251d864c36fdfd5
shaneneiman <shane.neiman@gmail.com>
2021-03-25 14:46:21 -0400

    app work, routes component started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RoutesComponent } from './routes/routes.component';
+
+const routes: Routes = [
+  { path: '', component : RoutesComponent},
+  { path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
